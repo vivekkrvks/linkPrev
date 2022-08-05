d7d000e6558b3688e8bfe2c7126523fa5250b055
Kumar <vivek.11.kumar.ext@nokia.com>
2022-08-05 17:41:24 +0530

    added condition if not able to find value from url:

--- pages/SearchResult.js
+++ pages/SearchResult.js
@@ -12,16 +12,19 @@ const SearchResult = () => {
   const router = useRouter();
     const { searchData } = router.query;
     const [resData,setResData] = useState({})
+    const [loading,setLoading] = useState(false)
     useEffect(() => {
       getData("");
     }, []);
-    const getData = async () => {    
+    const getData = async () => {  
+      setLoading(true)  
 let data = {link:searchData}
       await axios
         .post(`https://searchkarna.com/api/v1/addition/linkPrev/linkData`,data)
         .then((res) => (setResData(res.data)))
         .catch((err) => console.log(err));
-      console.log(resData)
+        setLoading(false)  
+
     };
   return (
     <>
@@ -36,10 +39,16 @@ let data = {link:searchData}
 </div>
 <div className="flex">
 
-<ResultComp 
+{(resData?.url && !loading)&&(<ResultComp 
 resData={resData}
 thumbImage={resData?.images?.[0]}
-/>
+/>)}
+{(loading)&&(<div class="flex justify-center items-center">
+  <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+</div>)}
+
   {/* middle */}
   
   {/* prev */}
