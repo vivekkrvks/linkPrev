899f9d426c2e6368a7a627c5da183be64c605da5
Kumar <vivek.11.kumar.ext@nokia.com>
2022-08-05 18:07:43 +0530

    loading msg

--- pages/SearchResult.js
+++ pages/SearchResult.js
@@ -22,7 +22,7 @@ let data = {link:searchData}
       await axios
         .post(`https://searchkarna.com/api/v1/addition/linkPrev/linkData`,data)
         .then((res) => (setResData(res.data)))
-        .catch((err) => console.log(err));
+        .catch((err) => (console.log(err)));
         setLoading(false)  
 
     };
@@ -39,16 +39,19 @@ let data = {link:searchData}
 </div>
 <div className="flex">
 
-{(resData?.url )&&(<ResultComp 
+{(resData?.url && !loading)&&(<ResultComp 
 resData={resData}
 thumbImage={resData?.images?.[0]}
 />)}
-{(loading)&&(<div class="flex justify-center items-center">
-  <div class="spinner-grow inline-block w-8 h-8 bg-current rounded-full opacity-0" role="status">
-    <span class="visually-hidden">Loading...</span>
-  </div>
-</div>)}
-{(!(resData?.url))&&(
+{(loading)&&(
+  <div className='mt-10 mb-20 flex-col justify-center items-center'>
+<h6 className="text-4xl md:text-6xl ml-4 font-bold">
+          Loading    <a className="text-blue-600" href={resData?.url}>
+             Please Wait ... 
+          </a>
+        
+        </h6></div>)}
+{(!(resData?.url)&& !loading)&&(
   <div className='mt-10 mb-20 flex-col justify-center items-center'>
 <h6 className="text-4xl md:text-6xl ml-4 font-bold">
           Sorry  Not able to get data from this link    <a className="text-blue-600" href={resData?.url}>
